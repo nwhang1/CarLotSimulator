84af1513977f68171f1fecd40af6a3871d07e62f
nwhang1 <nicholasjwhang@gmail.com>
2024-04-03 10:09:35 -0700

    added get set

--- CarLotSimulatorApp/CarLotSimulator/Car.cs
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -8,7 +8,7 @@ namespace CarLotSimulator
 {
     internal class Car
     {
-        public int Year;
+        public int Year { get; set; } 
         public string Make { get; set; }
         public string Model { get; set; }
         public string EngineNoise { get; set; }
